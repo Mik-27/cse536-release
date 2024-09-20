5516350215ab21fc40bf1a60be9ef9f631d585f7
Mihir Thakur <tamihir27@yahoo.com>
2024-09-20 11:28:52 -0700

    Update start.c

--- xv6-riscv/bootloader/start.c
+++ xv6-riscv/bootloader/start.c
@@ -140,6 +140,8 @@ void start()
   sys_info_ptr = (struct sys_info*)0x80080000;
   sys_info_ptr->bl_start = 0x80000000;
   sys_info_ptr->bl_end = end;
+  sys_info_ptr->dr_start = 0x80000000;
+  sys_info_ptr->dr_end = PHYSTOP;
  
  // out:
   /* CSE 536: Provide system information to the kernel. */
