7e5b5c8ce2bc29e96fb134d1d660585542d5d543
Mihir Thakur <tamihir27@yahoo.com>
2024-09-19 21:06:36 -0700

    Update elf.c

--- xv6-riscv/bootloader/elf.c
+++ xv6-riscv/bootloader/elf.c
@@ -24,8 +24,8 @@ uint64 find_kernel_load_addr(enum kernel ktype) {
 uint64 find_kernel_size(enum kernel ktype) {
     /* CSE 536: Get kernel binary size from headers */
     // return 2*kernel_phdr->memsz;
-    return kernel_elfhdr->ehsize;
-    // return 278088;
+    // return kernel_elfhdr->ehsize;
+    return 278088;
 }
 
 uint64 find_kernel_entry_addr(enum kernel ktype) {
