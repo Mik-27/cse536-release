3ca822ad5767680a40c2c7ac26cfb063339e5bf6
Mihir Thakur <tamihir27@yahoo.com>
2024-09-22 23:49:11 -0700

    Update start.c

--- xv6-riscv/bootloader/start.c
+++ xv6-riscv/bootloader/start.c
@@ -84,7 +84,7 @@ void start()
 
   /* CSE 536: With kernelpmp1, isolate upper 10MBs using TOR */ 
   #if defined(KERNELPMP1)
-    w_pmpaddr0(0x00000000874fffffull >> 2);
+    w_pmpaddr0(0x00000000874fffffull);
     w_pmpcfg0(0x0b);
   #endif
 
